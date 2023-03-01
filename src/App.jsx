ddaf162608a72b1eb912319665c2ce4d11607107
6 - uy ishi <jaloliddinabdusattorov83@gmail.com>
2023-03-01 11:11:16 +0500

    fifty persent

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useRef, useState } from 'react';
+import test from './question'
+import "./app.scss"
+
+function App() {
+  const btnRef = useRef()
+  const contRef = useRef()
+  const emptyRef = useRef()
+  const parentRef = useRef()
+
+  const [countTrue, setCountTrue] = useState(0)
+  const [countFalse, setCountFalse] = useState(0)
+
+  function submit(e){
+    if(e.target.className == "btn"){
+      e.target.parentElement.parentElement.style.display = "none"
+      if(e.target.dataset.type == "true"){
+        console.log("Success");
+        setCountTrue(countTrue + 1)
+      }else{
+        console.log("ERROR");
+        setCountFalse(countFalse + 1)
+      }
+    }
+    if(contRef.current.innerText == ""){
+      emptyRef.current.style.display = "block"
+    }
+  }
+  return (
+    <div ref={contRef} className="App">
+      {test.map((item)=>(
+      <div ref={parentRef} key={item.id} onClick={submit} className="card">
+        <h2>{item.test}</h2>
+        <div className="btn-group">
+          <button className='btn' ref={btnRef} data-type={item.A.type}>{item.A.answer}</button>
+          <button className='btn' ref={btnRef} data-type={item.B.type}>{item.B.answer}</button>
+          <button className='btn' ref={btnRef} data-type={item.C.type}>{item.C.answer}</button>
+        </div>
+      </div>
+      ))}
+
+<div ref={emptyRef} className='empty'>
+          <h1>Your result</h1>
+          <h3>
+            <span>True: {countTrue} </span>
+            <span>False: {countFalse} </span>
+          </h3>
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/question.js
@@ -0,0 +1,244 @@
+const test = [
+    {
+        id:1,
+        test: "What kapital city in uzbekistan",
+        A: {
+            answer: "Andijan",
+            type: false
+        },
+        B: {
+            answer: "Toshkent",
+            type: true
+        },
+        C: {
+            answer: "Samarkand",
+            type: false
+        },
+    },
+    {
+        id:2,
+        test: "What color the sky",
+        A: {
+            answer: "Blue",
+            type: true
+        },
+        B: {
+            answer: "red",
+            type: false
+        },
+        C: {
+            answer: "green",
+            type: false
+        },
+    },
+    {
+        id:3,
+        test: "Which car more expensive than this all car",
+        A: {
+            answer: "LaFerrari",
+            type: false
+        },
+        B: {
+            answer: "Bugatti",
+            type: false
+        },
+        C: {
+            answer: "Jentra",
+            type: true
+        },
+    },
+    {
+        id:4,
+        test: "What is the capital of France",
+        A: {
+            answer: "LISSABON",
+            type: false
+        },
+        B: {
+            answer: "Paris",
+            type: true
+        },
+        C: {
+            answer: "Luvr",
+            type: false
+        },
+    },
+    {
+        id:5,
+        test: "Full name HTML",
+        A: {
+            answer: "Hyper Text Markup Language",
+            type: true
+        },
+        B: {
+            answer: "Hyper Texture Marup Lang",
+            type: false
+        },
+        C: {
+            answer: "Hyper Text Marker Language",
+            type: false
+        },
+    },
+    {
+        id:6,
+        test: "Most expensive car in the world",
+        A: {
+            answer: "Rolls Royce Boat Tail",
+            type: false
+        },
+        B: {
+            answer: "Bugatti La Voiture Noire",
+            type: true
+        },
+        C: {
+            answer: "Pagani Zonda HP Barchetta",
+            type: false
+        },
+    },
+    {
+        id:7,
+        test: "The richest man in the world",
+        A: {
+            answer: "Jeff Bezos",
+            type: false
+        },
+        B: {
+            answer: "Ilon Mask",
+            type: false
+        },
+        C: {
+            answer: "Bernard Arnault ",
+            type: true
+        },
+    },
+    {
+        id:8,
+        test: "Full title is CSS",
+        A: {
+            answer: "Cascading Style",
+            type: false
+        },
+        B: {
+            answer: "Cascading Style Sheets",
+            type: true
+        },
+        C: {
+            answer: "Cascading Sheets Style",
+            type: false
+        },
+    },
+    {
+        id:9,
+        test: "What the old capital is Uzbekistan",
+        A: {
+            answer: "Samarkhand",
+            type: true
+        },
+        B: {
+            answer: "Andijan",
+            type: false
+        },
+        C: {
+            answer: "Bishkent",
+            type: false
+        },
+    },
+    {
+        id:10,
+        test: "The first prezident of Uzbekistan",
+        A: {
+            answer: "V.Garbachov",
+            type: false
+        },
+        B: {
+            answer: "Sharof Rashidov",
+            type: false
+        },
+        C: {
+            answer: "Islom Karimov",
+            type: true
+        },
+    },
+    {
+        id:11,
+        test: "The first prezident of U.S.A",
+        A: {
+            answer: "Auram Linkolin",
+            type: false
+        },
+        B: {
+            answer: "George Washington",
+            type: true
+        },
+        C: {
+            answer: "Benjamin Franklin",
+            type: false
+        },
+    },
+    {
+        id:12,
+        test: "The most favourite people in the World. All time",
+        A: {
+            answer: "Prophet Muhammad (s.a.w.)",
+            type: true
+        },
+        B: {
+            answer: "Prophet Jesus (a.s.)",
+            type: false
+        },
+        C: {
+            answer: "Isaak Nyuton",
+            type: false
+        },
+    },
+    {
+        id:13,
+        test: "Creator company Microsoft ",
+        A: {
+            answer: "Mark Zukkerberg",
+            type: false
+        },
+        B: {
+            answer: "Nikola Tesla",
+            type: false
+        },
+        C: {
+            answer: "Bill Geyts",
+            type: true
+        },
+    },
+    {
+        id:14,
+        test: "The most big country in the world",
+        A: {
+            answer: "United State of America",
+            type: false
+        },
+        B: {
+            answer: "Russia",
+            type: true
+        },
+        C: {
+            answer: "China",
+            type: false
+        },
+    },
+    {
+        id:15,
+        test: "The most high place in the world",
+        A: {
+            answer: "Mountain Pomir",
+            type: false
+        },
+        B: {
+            answer: "Himalay",
+            type: true
+        },
+        C: {
+            answer: "Dragon mountain place",
+            type: false
+        },
+    },
+]
+
+export default test
